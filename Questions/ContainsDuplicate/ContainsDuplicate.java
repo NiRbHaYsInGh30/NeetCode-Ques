2763c01e0a89a87c545fef3624ba7e1666ca53fb
Nirbhay Singh <90407590+NiRbHaYsInGh30@users.noreply.github.com>
2023-12-29 23:15:53 +0530

    4

--- Questions/ContainsDuplicate/ContainsDuplicate.java
+++ Questions/ContainsDuplicate/ContainsDuplicate.java
@@ -1,7 +1,12 @@
 //First Sort the array
 //AFter that itreate and check if any duplicate is present or not
 //If yes return true else false
- public boolean containsDuplicate(int[] nums) {
+
+
+
+
+/* 
+public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
       for(int i=0;i<nums.length-1;i++){
           if(nums[i]==nums[i+1]){
@@ -11,3 +16,4 @@
 
     return false;
     }
+    */
